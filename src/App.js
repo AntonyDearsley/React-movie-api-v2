ace501f60173cfbedf4e0d1634f91b60d451129b
Antony <antony.fernandez.dearsley@gmail.com>
2022-04-29 22:00:19 +0200

    Refactor of Search Movie and Series

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react'
 import './App.css';
 import { Route } from "wouter"
 import Home from "./pages/Home"
-import SearchAll from './pages/SearchAll'
+import SearchMovies from './pages/SearchMovies'
+import SearchSeries from './pages/SearchSeries';
 import Error404 from './pages/404'
 import Movies from './pages/Movies';
 import Series from './pages/Series';
 import Games from './pages/Games';
+
 
 export default function App() {
   return <>
@@ -31,9 +33,13 @@ export default function App() {
       />  
 
       <Route
-        component={SearchAll}
-        path="/search/:keyword" 
-       
+        component={SearchMovies}
+        path="/search/movie/:keyword" 
+      />
+
+      <Route
+        component={SearchSeries}
+        path="/search/serie/:keyword" 
       />
 
       
