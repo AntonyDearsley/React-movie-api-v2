c414f32e97f3c6d46559939a3aa7841d956129cf
Antony <antony.fernandez.dearsley@gmail.com>
2022-05-01 00:56:51 +0200

    Now the elements have handdleClick to details

--- src/components/Element/index.js
+++ src/components/Element/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { useLocation } from 'wouter';
 
-export default function Element (params) {
-    return <div id={params.id} key={params.id}>
+export  default  function Element (params) {
+    const [, pathLocation] = useLocation()
+    const condition = ()=> params.title === undefined ? params.name : params.title
+    
+    const handdleClick = ()=> {
+        params.type  === "movie" ? pathLocation(`/movie/detail/${params.id}`)
+        : pathLocation(`/404`)
+    }
+      
+    return <div id={params.id} key={params.id} onClick={handdleClick}>
         <img 
-        className='poster' 
+        className='poster' title={`${params.type}-${()=>condition()}`} 
         src={params.image} 
-        alt={`Element-Movie: ${params.title}`} 
-        />
+        alt={`Element-${params.type}: ${condition()}`} 
+         />
     </div>
 }
 
