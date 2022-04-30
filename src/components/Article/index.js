b6d112e12eb8f05611943bfbd2d65324854527ec
Antony <antony.fernandez.dearsley@gmail.com>
2022-05-01 00:56:08 +0200

    Refactor article to add css

--- src/components/Article/index.js
+++ src/components/Article/index.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { useLocation } from 'wouter'
 
-export default function Article({ children , url}) {
+export default function Article({ children , url, cssArticle }) {
   const [, pathLocation] = useLocation()
   
   const handleClick = () => {
     pathLocation(url)
   }
 
-  return <div key={url} className='article' onClick={handleClick}>
+  return <div key={url} className={`article ${cssArticle}`} onClick={handleClick}>
     {children}
   </div>
   
