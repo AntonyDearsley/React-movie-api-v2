b39d1f28b1b5b1525669642eacdec15c52ae7fd1
Antony <antony.fernandez.dearsley@gmail.com>
2022-05-01 01:00:05 +0200

    Refactor SearchMovie and SearchSeries

--- src/pages/SearchMovies/index.js
+++ src/pages/SearchMovies/index.js
@@ -2,7 +2,7 @@ import ListOfElements from '../../components/ListOfElements'
 import Button from '../../components/Button'
 import Menu from '../../components/Menu'
 import { useHandleClickMovie } from '../../hooks/handleClickMovie'
-import Error404 from '../404'
+import E404 from '../../components/404'
 
 
 
@@ -20,7 +20,14 @@ export default function SearchMovies({ params }) {
     
 
     if (parameter.length === 0){
-        return <Error404 />
+        return <div id='container' className='bg-zinc-900 h-full text-white flex flex-col items-center'>
+            <Menu list={list} css={"menuH"} cssArticles={"articlesH"}/>
+            <div className='bg-[#1b1b1b] h-screen w-full 
+            relative flex justify-center items-center overflow-x-hidden'>
+                <E404 />
+            </div>
+        </div>
+        
     }
 
     return <div id='container' className='bg-zinc-900 h-full text-white flex flex-col items-center'>
--- src/pages/SearchSeries/index.js
+++ src/pages/SearchSeries/index.js
@@ -1,8 +1,8 @@
 import ListOfElements from '../../components/ListOfElements'
 import Button from '../../components/Button'
 import { useHandleClickSeries } from '../../hooks/handleClickSeries'
-import Error404 from '../404'
 import Menu from '../../components/Menu'
+import E404 from '../../components/404'
 
 export default function SearchSeries({ params }) {
     const keyword  = params
@@ -17,11 +17,17 @@ export default function SearchSeries({ params }) {
     
     
     if (parameter.length === 0) {
-        return <Error404 />
+        return <div id='container' className='bg-zinc-900 h-full text-white flex flex-col items-center'>
+            <Menu list={list} css={"menuH"} cssArticles={"articlesH"} />
+            <div className='bg-[#1b1b1b] h-screen w-full 
+                relative flex justify-center items-center overflow-x-hidden' >
+                <E404 />
+            </div>
+        </div>
     }
 
     return <div id='container' className='bg-zinc-900 h-full text-white flex flex-col items-center'>
-            <Menu list={list} css={"menuH"} cssArticles={"articlesH"}/>
+            <Menu list={list} css={"menuH"} cssArticles={"articlesH"} />
             <div className='container'>
                 <ListOfElements element={parameter} />
             </div>
@@ -30,6 +36,5 @@ export default function SearchSeries({ params }) {
                 <Button handleClick={handleClick}>Más Series</Button>
             </div>
         </div>
-
 }
     
