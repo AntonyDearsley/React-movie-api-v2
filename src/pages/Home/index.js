c9d7c1766f1e44d2785737d3039da08cf0f32f26
Antony <antony.fernandez.dearsley@gmail.com>
2022-05-04 01:18:58 +0200

    Adding carousle component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,9 +4,12 @@ import Menu from '../../components/Menu'
 import "./index.css"
 import { faSearch } from '@fortawesome/fontawesome-free-solid'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useTitle } from '../../hooks/useTitle'
+import 'tw-elements';
+import Carousel from '../../components/Carousel'
 
 const list = {
-    'HOME': '/', 
+    'HOME': '/',
     'MOVIES': '/movie',
     'SERIES': '/serie',
     'GAMES': '/game'
@@ -15,28 +18,38 @@ const list = {
 export default function Home() {
     const [keyword, setKeyword] = useState('')
     const [, pushLocation] = useLocation()
+    const parameter = useTitle()
 
     const handleSubmit = e => {
         e.preventDefault()
-        keyword === '' ? pushLocation('/404')  
-        : pushLocation(`/search/${keyword}`)
+        keyword === '' ? pushLocation('/404')
+            : pushLocation(`/search/${keyword}`)
     }
 
     const handleChange = e => {
         setKeyword(e.target.value)
     }
 
-    return  <div id='container' className='home'>
-                <Menu list={list} css={"menuV"} cssArticles={"articlesV"}/>
-                <div className='h-screen sm:w-screen flex justify-center items-center'>
-                    <form onSubmit={handleSubmit} className="relative">
-                        <input placeholder=" Search a title here..." onChange={handleChange} 
-                        type='text' value={keyword} className='rounded-full p-3 
+    console.log(parameter.results)
+
+    return <div id='container' className='home'>
+        <Menu list={list} css={"menuV"} cssArticles={"articlesV"} />
+        <div className='h-screen sm:w-screen flex flex-col items-center'>
+            
+        <div className='w-[1000px] my-14'>
+            <Carousel />
+        </div>
+            
+            
+            
+            <form onSubmit={handleSubmit} className="relative">
+                <input placeholder=" Search a title here..." onChange={handleChange}
+                    type='text' options={parameter.results} value={keyword} className='rounded-full p-3 
                         w-[20rem] sm:w-[23rem] lg:w-[38rem] outline-double 
                         focus:outline-8 focus:outline-white font-Montserrat' />
-                        <FontAwesomeIcon icon={faSearch} className="text-black text-xl absolute 
+                <FontAwesomeIcon icon={faSearch} className="text-black text-xl absolute 
                         right-5 top-3"/>
-                    </form>
-                </div>
-            </div>
+             </form>
+        </div>
+    </div>
 }
