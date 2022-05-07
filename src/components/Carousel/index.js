e62adebcf0a68f82b4d3e145c8805e1b7a0369e5
Antony <antony.fernandez.dearsley@gmail.com>
2022-05-07 21:00:38 +0200

    Deleting Trash

--- src/components/Carousel/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import CarouselElements from '../Carousel-Elements'
-
-export default function Carousel({ list, cssSlides, cssCarousel, cssImages }) {
-  return <div className={cssCarousel}>
-      <CarouselElements cssSlides={cssSlides} 
-      list={list} cssImages={cssImages}/>
-  </div>
-}
