c2bfbbd57159808ef53283d68bbb1331cfbe8b49
Antony <antony.fernandez.dearsley@gmail.com>
2022-05-14 03:09:28 +0200

    changing SerieCredists to es-ES

--- src/services/getSerieCredits/index.js
+++ src/services/getSerieCredits/index.js
@@ -3,7 +3,7 @@ import getImage from "../getImage"
 const api_key = 'b677204d99e9870416faefa56773d56c'
 
 export default async function getSerieCredits({ id }) {
-    const apiURL = `https://api.themoviedb.org/3/tv/${id}/credits?api_key=${api_key}&language=es-ES`
+    const apiURL = `https://api.themoviedb.org/3/tv/${id}/credits?api_key=${api_key}&language=en-EN`
 
 
     return fetch(apiURL)
