7980321df564ae5f41afd1a18dc88bace96a4780
Antony <antony.fernandez.dearsley@gmail.com>
2022-05-18 21:43:14 +0200

    change config database

--- Server/index.js
+++ Server/index.js
@@ -8,7 +8,7 @@ const db = mysql.createPool({
   host: 'mysql', // the host name MYSQL_DATABASE: node_mysql
   user: 'root', // database user MYSQL_USER: MYSQL_USER
   password: 'root', // database user password MYSQL_PASSWORD: MYSQL_PASSWORD
-  database: 'books' // database name MYSQL_HOST_IP: mysql_db
+  database: 'mobee' // database name MYSQL_HOST_IP: mysql_db
 })
 
 // Enable cors security headers
@@ -30,40 +30,74 @@ app.get('/example', (req, res) => {
 3
 
 
-// get all of the books in the database
-app.get('/get', (req, res) => {
-  const SelectQuery = " SELECT * FROM  books_reviews";
+// get all of the users in the database
+app.get('/get/allUsers', (req, res) => {
+  const SelectQuery = " SELECT * FROM USUARIOS";
   db.query(SelectQuery, (err, result) => {
     res.send(result)
   })
 })
 
-// add a book to the database
-app.post("/insert", (req, res) => {
-  const bookName = req.body.setBookName;
-  const bookReview = req.body.setReview;
-  const InsertQuery = "INSERT INTO books_reviews (book_name, book_review) VALUES (?, ?)";
-  db.query(InsertQuery, [bookName, bookReview], (err, result) => {
-    console.log(result)
+// add a user to the database
+app.get("/insert/user/:name/:email/:password", (req, res) => {
+  // const bookName = req.body.setBookName;
+  // const bookReview = req.body.setReview;
+
+  const name = req.params.name;
+  const email = req.params.email;
+  const password = req.params.password;
+
+  const InsertQuery = "INSERT INTO USUARIOS (NOMBRE, CORREO, CONTRASENA) VALUES (?, ?, ?)";
+  db.query(InsertQuery, [name, email, password], (err) => {
+    if (err) res.send(err)
+    else res.send(`Has añadido el usuario con nombre: ${name}, correo: ${email} y contraseña: ${password}`)
   })
 })
 
-// delete a book from the database
-app.delete("/delete/:bookId", (req, res) => {
-  const bookId = req.params.bookId;
-  const DeleteQuery = "DELETE FROM books_reviews WHERE id = ?";
-  db.query(DeleteQuery, bookId, (err, result) => {
-    if (err) console.log(err);
+// delete a user from the database
+app.get("/delete/user:userId", (req, res) => {
+  const userId = req.params.userId;
+  const DeleteQuery = "DELETE FROM USUARIOS WHERE ID = ?";
+
+  db.query(DeleteQuery, userId, (err) => {
+    if (err) res.send(err);
+    else res.send(`Has borrado el usuario con id -> ${userId}`);
   })
 })
 
-// update a book review
-app.put("/update/:bookId", (req, res) => {
-  const bookReview = req.body.reviewUpdate;
-  const bookId = req.params.bookId;
-  const UpdateQuery = "UPDATE books_reviews SET book_review = ? WHERE id = ?";
-  db.query(UpdateQuery, [bookReview, bookId], (err, result) => {
-    if (err) console.log(err)
+// update a user name
+app.get("/update/user:userId/toName/:name", (req, res) => {
+  const userId = req.params.userId;
+  const name = req.params.name;
+  const UpdateQuery = "UPDATE USUARIOS SET NOMBRE = ? WHERE ID = ?";
+
+  db.query(UpdateQuery, [name, userId], (err) => {
+    if (err) res.send(err)
+    else res.send(`Has actualizado el nombre a ${name} del usuario con id ${userId}`)
+  })
+})
+
+// update a user email
+app.get("/update/user:userId/toEmail/:email", (req, res) => {
+  const userId = req.params.userId;
+  const email = req.params.email;
+  const UpdateQuery = "UPDATE USUARIOS SET CORREO = ? WHERE ID = ?";
+
+  db.query(UpdateQuery, [email, userId], (err) => {
+    if (err) res.send(err)
+    else res.send(`Has actualizado el correo a ${email} del usuario con id ${userId}`)
+  })
+})
+
+// update a user password
+app.get("/update/user:userId/toPassword/:password", (req, res) => {
+  const userId = req.params.userId;
+  const password = req.params.password;
+  const UpdateQuery = "UPDATE USUARIOS SET CONTRASENA = ? WHERE ID = ?";
+
+  db.query(UpdateQuery, [password, userId], (err) => {
+    if (err) res.send(err)
+    else res.send(`Has actualizado la contraseña a ${password} del usuario con id ${userId}`)
   })
 })
 
