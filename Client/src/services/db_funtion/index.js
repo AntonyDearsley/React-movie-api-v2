8dabe6dc72da913235abfcbb275c9d6a3f01401a
Antony <antony.fernandez.dearsley@gmail.com>
2022-05-22 15:41:40 +0200

    Creating more functions for database

--- Client/src/services/db_funtion/index.js
+++ Client/src/services/db_funtion/index.js
@@ -54,3 +54,96 @@ export async function getUser(param, password) {
         })
     }
 }
+
+export function validateRegister(params) {
+    const username = params.username
+    const email = params.email
+    const password = params.password
+    const password2 = params.password2
+
+    if(username.length < 1 && 
+        email.length < 1 &&
+         password.length < 1 &&
+          password2.length < 1) {
+        return {res: false , message: 'Los contenedores no pueden estar vacio.'}
+    } else if (username.includes('@')) {
+        return {res: false , message: 'El usuario no debe contener el caracter: @'}
+    } else if (!email.includes('@') || email < 5) {
+        return {res: false , message: 'El email no es valido.' }
+    } else if (password !== password2) {
+        return {res: false , message: 'Las contraseñas no coinciden.'}
+    } else if (password < 8) {
+        return {res: false , message: 'Las contraseña no puede ser menor de 8 caracteres.'}
+    }
+    else {
+        return {res: true , message: 'OK'}
+    }
+
+}
+
+export async function insertUser(params) {
+    const sentence = apiURL + `insert/user`
+
+    return fetch(sentence, {
+        method: 'POST',
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(params)
+    }).then(response => {
+        return response.status === 200 ? 'true' : 'false'
+    })
+}
+
+export async function insertFav(params) {
+    const sentence = apiURL + `insert/fav/multimedia`
+
+    console.log(params)
+
+    return fetch(sentence, {
+        method: 'POST',
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(params)
+    }).then(response => {
+        return response.status === 200 ? 'true' : 'false'
+    })
+}
+
+export async function deletetFav(params) {
+    const sentence = apiURL + `delete/fav/multimedia`
+
+    return fetch(sentence, {
+        method: 'DELETE',
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(params)
+    }).then(response => {
+        return response.status === 200 ? 'true' : 'false'
+    })
+}
+
+export async function searchMultimediaFav(params) {
+    const sentence = apiURL + `search/fav/multimedia`
+    
+    return fetch(sentence, {
+        method: 'POST',
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(params)
+    })
+    .then(res => res.json())
+    .then(response => {
+        console.log(response)
+        return response.length > 0 ? true : false
+    })
+}
+
+
+export async function insertList(params) {
+    const sentence = apiURL + `insert/list/multimedia`
+
+    fetch(sentence, {
+        method: 'POST',
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(params)
+    }).then(response => {
+        return response.status === 200 ? 'true' : 'false'
+    })
+}
+
