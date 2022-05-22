3e9ee95d669cdf41b6b3055bdf51d7d8d9020888
Antony <antony.fernandez.dearsley@gmail.com>
2022-05-22 15:42:13 +0200

    Refactoring express

--- Server/index.js
+++ Server/index.js
@@ -18,110 +18,195 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
-// home page
-app.get('/', (req, res) => {
-  res.send('Hi There')
-});
-
-// example page
-app.get('/example', (req, res) => {
-  res.send('example')
-});
-3
-
-
-// get all of the users in the database
+// get all of the users in the database (by GET method)
 app.get('/get/allUsers', (req, res) => {
-  const SelectQuery = " SELECT * FROM USUARIOS";
+  const SelectQuery = "SELECT * FROM USUARIOS";
   db.query(SelectQuery, (err, result) => {
-    res.send(result)
+    if (err) res.status(400).send('BAD REQUEST')
+    else res.send(result)
   })
 })
 
-// get all of one user in the database
-app.get('/get/user/name=:name', (req, res) => {
-  const name = req.params.name;
+// get all the data of one user in the database by name (by POST method)
+app.post('/get/user/name', (req, res) => {
+  const name = req.body.name;
   const SelectQuery = "SELECT * FROM USUARIOS WHERE NOMBRE = ?";
   db.query(SelectQuery, name ,(err, result) => {
-    if (err) res.send(err)
+    if (err) res.status(400).send('BAD REQUEST')
     else res.send(result)
   })
 })
 
-// get all of one user in the database
-app.get('/get/user/email=:email', (req, res) => {
-  const email = req.params.email;
+// get all the data of one user in the database by email (by POST method)
+app.post('/get/user/email', (req, res) => {
+  const email = req.body.email;
   const SelectQuery = "SELECT * FROM USUARIOS WHERE CORREO = ?";
   db.query(SelectQuery, email ,(err, result) => {
-    if (err) res.send(err)
+    if (err) res.status(400).send('BAD REQUEST')
     else res.send(result)
   })
 })
 
-
-
-
-// add a user to the database
-app.get("/insert/user/:name/:email/:password", (req, res) => {
-  // const bookName = req.body.setBookName;
-  // const bookReview = req.body.setReview;
-
-  const name = req.params.name;
-  const email = req.params.email;
-  const password = req.params.password;
+// add a user to the database (by POST method)
+app.post("/insert/user", (req, res) => {
+  const name = req.body.username;
+  const email = req.body.email;
+  const password = req.body.password;
 
   const InsertQuery = "INSERT INTO USUARIOS (NOMBRE, CORREO, CONTRASENA) VALUES (?, ?, ?)";
   db.query(InsertQuery, [name, email, password], (err) => {
-    if (err) res.send(err)
-    else res.send(`Has añadido el usuario con nombre: ${name}, correo: ${email} y contraseña: ${password}`)
+    if (err) res.status(400).send('BAD REQUEST')
+    else res.status(200).send('OK')
   })
 })
 
 // delete a user from the database
-app.get("/delete/user:userId", (req, res) => {
-  const userId = req.params.userId;
+app.delete("/delete/user/id", (req, res) => {
+  const userId = req.body.userId;
   const DeleteQuery = "DELETE FROM USUARIOS WHERE ID = ?";
 
   db.query(DeleteQuery, userId, (err) => {
-    if (err) res.send(err);
-    else res.send(`Has borrado el usuario con id -> ${userId}`);
+    if (err) res.status(400).send('BAD REQUEST')
+    else res.status(200).send('OK')
   })
 })
 
 // update a user name
-app.get("/update/user:userId/toName/:name", (req, res) => {
-  const userId = req.params.userId;
-  const name = req.params.name;
+app.put("/update/user/Name", (req, res) => {
+  const userId = req.body.userId;
+  const name = req.body.name;
   const UpdateQuery = "UPDATE USUARIOS SET NOMBRE = ? WHERE ID = ?";
 
   db.query(UpdateQuery, [name, userId], (err) => {
-    if (err) res.send(err)
-    else res.send(`Has actualizado el nombre a ${name} del usuario con id ${userId}`)
+    if (err) res.status(400).send('BAD REQUEST')
+    else res.status(200).send('OK')
   })
 })
 
 // update a user email
-app.get("/update/user:userId/toEmail/:email", (req, res) => {
-  const userId = req.params.userId;
-  const email = req.params.email;
+app.put("/update/user/Email", (req, res) => {
+  const userId = req.body.userId;
+  const email = req.body.email;
   const UpdateQuery = "UPDATE USUARIOS SET CORREO = ? WHERE ID = ?";
 
   db.query(UpdateQuery, [email, userId], (err) => {
-    if (err) res.send(err)
-    else res.send(`Has actualizado el correo a ${email} del usuario con id ${userId}`)
+    if (err) res.status(400).send('BAD REQUEST')
+    else res.status(200).send('OK')
   })
 })
 
 // update a user password
-app.get("/update/user:userId/toPassword/:password", (req, res) => {
-  const userId = req.params.userId;
-  const password = req.params.password;
+app.put("/update/user/Password/", (req, res) => {
+  const userId = req.body.userId;
+  const password = req.body.password;
   const UpdateQuery = "UPDATE USUARIOS SET CONTRASENA = ? WHERE ID = ?";
 
   db.query(UpdateQuery, [password, userId], (err) => {
-    if (err) res.send(err)
-    else res.send(`Has actualizado la contraseña a ${password} del usuario con id ${userId}`)
+    if (err) res.status(400).send('BAD REQUEST')
+    else res.status(200).send('OK')
   })
 })
+
+// get all the multimedia of one user in the database by id (by POST method)
+app.post('/get/fav/multimedia', (req, res) => {
+  const id = req.body.id;
+  const SelectQuery = "SELECT * FROM FAVORITOS WHERE ID_USUARIO = ?";
+  db.query(SelectQuery, id ,(err, result) => {
+    if (err) res.status(400).send('BAD REQUEST')
+    else res.send(result)
+  })
+})
+
+app.post('/search/fav/multimedia', (req, res) => {
+  const type = req.body.type;
+  const id_multi= req.body.id_multi;
+  const id_user= req.body.id_user;
+  const SelectQuery = "SELECT * FROM FAVORITOS WHERE TIPO = ? AND ID_MULTIMEDIA = ? AND ID_USUARIO = ?";
+  db.query(SelectQuery, [type, id_multi, id_user] ,(err, result) => {
+    if (err) res.status(400).send('BAD REQUEST')
+    else res.send(result)
+  })
+})
+
+// add a multimedia to fav table in the database 
+app.post("/insert/fav/multimedia", (req, res) => {
+  const title = req.body.title;
+  const type = req.body.type;
+  const id_multi= req.body.id_multi;
+  const id_user= req.body.id_user;
+
+  const InsertQuery = "INSERT INTO FAVORITOS (NOMBRE, TIPO, ID_MULTIMEDIA, ID_USUARIO) VALUES (?, ?, ?, ?)";
+  db.query(InsertQuery, [title, type, id_multi, id_user], (err) => {
+    if (err) res.status(400).send('BAD REQUEST')
+    else res.status(200).send('OK')
+  })
+})
+
+// delete a multimedia from fav table of the database
+app.delete("/delete/fav/multimedia", (req, res) => {
+  const type = req.body.type;
+  const id_multi= req.body.id_multi;
+  const id_user= req.body.id_user;
+
+  const DeleteQuery = 
+  "DELETE FROM FAVORITOS WHERE TIPO = ? AND ID_MULTIMEDIA = ? AND ID_USUARIO = ?";
+
+  db.query(DeleteQuery, [type, id_multi, id_user], (err) => {
+    if (err) res.status(400).send('BAD REQUEST')
+    else res.status(200).send('OK')
+  })
+})
+
+// get all the multimedia of one user in the database by id (by POST method)
+app.post('/get/list/multimedia', (req, res) => {
+  const id = req.body.id;
+  const SelectQuery = "SELECT * FROM LISTA WHERE ID_USUARIO = ?";
+  db.query(SelectQuery, id ,(err, result) => {
+    if (err) res.status(400).send('BAD REQUEST')
+    else res.send(result)
+  })
+})
+
+// add a multimedia to list table in the database
+app.post("/insert/list/multimedia", (req, res) => {
+  const name = req.body.name;
+  const type = req.body.type;
+  const id_multi= req.body.id_multi;
+  const id_user= req.body.id_user;
+
+  const InsertQuery =
+  "INSERT INTO LISTA (NOMBRE, TIPO, ID_MULTIMEDIA,  ID_USUARIO) VALUES (?, ?, ?, ?)";
+  db.query(InsertQuery, [name, type, id_multi, id_user], (err) => {
+    if (err) res.status(400).send('BAD REQUEST')
+    else res.status(200).send('OK')
+  })
+})
+
+// delete a multimedia from fav list of the database
+app.delete("/delete/list/multimedia", (req, res) => {
+  const user_id = req.body.user_id;
+  const DeleteQuery = "DELETE FROM LISTA WHERE ID_USUARIO = ?";
+
+  db.query(DeleteQuery, user_id, (err) => {
+    if (err) res.status(400).send('BAD REQUEST')
+    else res.status(200).send('OK')
+  })
+})
+
+
+
+
+
+
+
+// get all fav in the database (by GET method)
+app.get('/get/fav/', (req, res) => {
+  const SelectQuery = "SELECT * FROM FAVORITOS";
+  db.query(SelectQuery, (err, result) => {
+    if (err) res.status(400).send('BAD REQUEST')
+    else res.send(result)
+  })
+})
+
 
 app.listen('3001', () => { })
