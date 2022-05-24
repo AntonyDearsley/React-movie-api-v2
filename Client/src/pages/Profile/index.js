667f091af4203e6c48eb416ba8e8ef19a9a05e86
Antony <antony.fernandez.dearsley@gmail.com>
2022-05-24 22:16:11 +0200

    Refactor Profile page

--- Client/src/pages/Profile/index.js
+++ Client/src/pages/Profile/index.js
@@ -1,28 +1,62 @@
-import { faLongArrowAltLeft } from '@fortawesome/fontawesome-free-solid';
+import { faEye, faEyeSlash, faLongArrowAltLeft, faTrash } from '@fortawesome/fontawesome-free-solid';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useState } from 'react'
 import { useLocation } from 'wouter';
 import { useFav } from '../../hooks/useFav';
 import { useList } from '../../hooks/useList';
 import { useProfile } from '../../hooks/useProfile';
+import * as db from '../../services/db_funtion';
 
 export default function Profile() {
     const [, pathLocation] = useLocation()
     const fav = useFav({ id_user: localStorage.getItem('IdSesion') })
     const list = useList({ id_user: localStorage.getItem('IdSesion') })
     const profile = useProfile({ id: localStorage.getItem('IdSesion') })
-    const [password, setPassword] = useState(false)
+    const [view, setView] = useState(false)
 
-    console.log(profile)
     let countFav = 0
     let countList = 0
 
-    const handlePassword = () => {
-        setPassword(!password)
+    if(localStorage.getItem('IdSesion') === null){
+        pathLocation('/')
+    }
+
+    const handleView = () => {
+        setView(!view)
     }
 
     const handleHome = () => {
         pathLocation("/")
+    }
+
+    const handleDelete = (id, type) => {
+        const id_user = localStorage.getItem('IdSesion')
+        const id_multi = id
+        const tipo = type
+
+        db.deletetList({ type: tipo , id_multi: id_multi, id_user: id_user })
+    }
+
+    const handleSession = () => {
+        window.localStorage.clear()
+        pathLocation("/")
+    }
+
+    const handleAccount = () => {
+        const res = prompt("Escriba ELIMINAR si realmente desea eliminar la cuenta.")
+        
+        res === 'ELIMINAR'?
+        db.deleteUser({ id: localStorage.getItem('IdSesion') }).then(response => {
+            if (response === 'true') {
+                alert('Se ha borrado la cuenta con éxito')
+                window.localStorage.clear()
+                pathLocation('/')
+            } else {  
+                alert('Ha habido un error al borrar la cuenta')
+            }
+        })
+        :
+        alert('No se ha eliminado la cuenta')
     }
 
     const handleDetails = e => {
@@ -32,9 +66,9 @@ export default function Profile() {
         pathLocation(`/${tipo}/detail/${id}`)
     }
 
-    return <div className='h-screen bg-black flex flex-col'>
+    return <div className='h-screen bg-zinc-900 flex flex-col'>
 
-        <section className='w-full h-12 flex items-center  border-4 border-white'>
+        <section className='w-full h-12 flex items-center border-b border-b-zinc-200'>
             <div className='w-10 flex items-center text-zinc-100 hover:cursor-pointer absolute left-6'
                 onClick={handleHome}>
                 <FontAwesomeIcon icon={faLongArrowAltLeft} className='h-10 ' />
@@ -43,10 +77,10 @@ export default function Profile() {
         </section>
 
 
-        <div className='border-4 border-white h-screen flex '>
-            <div className='border-4 border-red-500  w-[25%] '>
+        <div className=' h-screen flex'>
+            <div className='p-6 w-[25%]  border-r border-zinc-200'>
                 <h1 className='text-2xl font-Montserrat text-zinc-100'>PROFILE</h1>
-                <div className='border border-pink-400 '>
+                <div className=''>
 
                     {
                         profile.loading === true ?
@@ -54,34 +88,52 @@ export default function Profile() {
                                 <span className="loader  " />
                             </div>
                             :
-                            <>
-                                <div>
-                                    <p> NOMBRE DE USUARIO: {profile.value[0].NOMBRE}</p>
-                                    <p> EMAIL: {profile.value[0].CORREO}</p>
+                            <div className='h-72'>
+                                <div className='text-zinc-200 flex flex-col justify-evenly h-72'>
                                     <div>
-                                        <p> CONTRASEÑA: {password === false ?
+                                        <p className='font-bold'> NOMBRE DE USUARIO</p>
+                                        <p>{profile.value[0].NOMBRE}</p>
+                                    </div>
+                                    
+                                    <div>
+                                        <p className='font-bold'> EMAIL </p> 
+                                        <p>{profile.value[0].CORREO}</p>
+                                    </div>
+                                    
+                                    <div className=''>
+                                        <p className='font-bold'> CONTRASEÑA</p> 
+                                        <p>
+                                            {
+                                            view ?
                                             '*'.repeat(profile.value[0].CONTRASENA.length)
-                                            : profile.value[0].CONTRASENA}</p>
-                                        <button onClick={handlePassword}>VER</button>
+                                            : profile.value[0].CONTRASENA
+                                            }
+                                        </p>
+                                        <span onClick={handleView} className='flex justify-center p-3 hover:cursor-pointer border rounded-md mt-2'>
+                                            <p className=' text-zinc-100'>{view ? 'Mostrar contraseña' : 'Ocultar contraseña'}</p>
+                                            <FontAwesomeIcon className='translate-y-1 translate-x-2 text-zinc-100' 
+                                            icon={view ? faEye : faEyeSlash} 
+                                            />
+                                        </span>
                                     </div>
                                 </div>
-                                <div>
-                                    <button>Cerrar Session</button>
-                                    <button>Eliminar Cuenta</button>
+                                <div className='flex flex-col justify-around h-20'>
+                                    <button className='font-bold font-Montserrat  border w-full border-zinc-100 rounded-md text-zinc-100 hover:cursor-pointer ' onClick={handleSession}>Cerrar Session</button>
+                                    <button className='font-bold font-Montserrat border border-red-600 w-full rounded-md bg-red-600 text-zinc-100 hover:cursor-pointer' onClick={handleAccount}>Eliminar Cuenta</button>
                                 </div>
-                            </>
+                            </div>
                     }
 
 
                 </div>
             </div>
-            <div className='border-4 border-blue-500 h-full w-[75%] flex relative'>
-                <div className='border-8 border-green-500  absolute h-full w-1/2'>
+            <div className=' h-full w-[75%] flex relative'>
+                <div className='p-6 absolute h-full w-1/2'>
                     <h1 className='text-2xl font-Montserrat text-zinc-100 text-center'>FAVORITOS</h1>
-                    <div className='border border-white max-h-[95%] overflow-auto'>
+                    <div className=' max-h-[95%] overflow-auto'>
                         {
                             fav.loading === false ?
-                                <table className='w-full'>
+                                <table className='w-full mt-2'>
 
                                     {
 
@@ -91,12 +143,12 @@ export default function Profile() {
                                                 countFav % 2 === 0 ? 'border bg-zinc-900'
                                                     : 'border bg-zinc-700'
                                             }>
-                                                <th id={result.ID_MULTIMEDIA} data-tipo={result.TIPO}
+                                                <td id={result.ID_MULTIMEDIA} data-tipo={result.TIPO}
                                                     className='text-zinc-200 font-light text-left hover:text-red-600 hover:cursor-pointer'
                                                     onClick={handleDetails}>
                                                     {result.TIPO === 'M' ? 'Película: ' : 'Serie: '}
                                                     {result.NOMBRE}
-                                                </th>
+                                                </td>
                                             </tr>
                                         })
                                     }
@@ -109,14 +161,14 @@ export default function Profile() {
                         }
                     </div>
                 </div>
-                <div className='border-8 border-orange-500 absolute h-full left-1/2 w-1/2'>
+                <div className='p-6 absolute h-full left-1/2 w-1/2 '>
                     <h1 className='text-2xl font-Montserrat text-zinc-100 text-center'>VER MÁS TARDE</h1>
-                    <div className='border border-white max-h-[95%] overflow-auto'>
+                    <div className=' max-h-[95%] overflow-auto'>
                         {
 
                             list.loading === false ?
 
-                                <table className='w-full'>
+                                <table className='w-full mt-2'>
 
                                     {
 
@@ -127,12 +179,16 @@ export default function Profile() {
                                                 countList % 2 === 0 ? 'border bg-zinc-900'
                                                     : 'border bg-zinc-700'
                                             }>
-                                                <th id={result.ID_MULTIMEDIA} data-tipo={result.TIPO}
+                                                <td id={result.ID_MULTIMEDIA} data-tipo={result.TIPO}
                                                     className='text-zinc-200 font-light text-left hover:text-red-600 hover:cursor-pointer'
                                                     onClick={handleDetails}>
                                                     {result.TIPO === 'M' ? 'Película: ' : 'Serie: '}
                                                     {result.NOMBRE}
-                                                </th>
+                                                </td>
+                                                <td className='flex justify-center items-center hover:cursor-pointer h-full'
+                                                onClick={()=>{handleDelete(result.ID_MULTIMEDIA, result.TIPO)}} >
+                                                    <FontAwesomeIcon className='text-zinc-100' icon={faTrash} />
+                                                </td>
                                             </tr>
                                         })
 
